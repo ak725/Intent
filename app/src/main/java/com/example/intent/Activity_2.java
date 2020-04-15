5d9d04d9df2ac00979d1bd36fe7c8cfa939840ba
ak725 <ajithkumarjeyanth2125@gmail.com>
2020-04-15 23:28:22 +0530

    Initial commit

---
+++ app/src/main/java/com/example/intent/Activity_2.java
@@ -0,0 +1,26 @@
+package com.example.intent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.app.VoiceInteractor;
+import android.os.Bundle;
+
+import javax.xml.transform.Result;
+
+@SuppressLint("Registered")
+public class Activity_2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_2);
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        setResult(RESULT_OK);
+        finish();
+    }
+}
